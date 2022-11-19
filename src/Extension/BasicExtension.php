0903cba1de2e30a540b098417ac7bc22ebfea404
MartkCz <martk494@gmail.com>
2022-11-19 15:12:41 +0100

    change viewport

--- src/Extension/BasicExtension.php
+++ src/Extension/BasicExtension.php
@@ -5,7 +5,6 @@ namespace WebChemistry\Metadata\Extension;
 use WebChemistry\Metadata\Html\Wrapper;
 use WebChemistry\Metadata\Metadata\BasicMetadata;
 use WebChemistry\Metadata\MetadataExtensionInterface;
-use WebChemistry\Metadata\Utility\MetadataUtility;
 
 final class BasicExtension implements MetadataExtensionInterface
 {
@@ -28,7 +27,7 @@ final class BasicExtension implements MetadataExtensionInterface
 	{
 		$wrapper->add('meta', ['charset' => 'utf-8']);
 		$wrapper->add('meta', ['http-equiv' => 'X-UA-Compatible', 'content' => 'IE=edge,chrome=1']);
-		$wrapper->addMetaWithName('viewport', 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no');
+		$wrapper->addMetaWithName('viewport', 'width=device-width, initial-scale=1');
 
 		if ($title = $this->basicMetadata->getTitle()) {
 			$wrapper->add('title', text: $title);
